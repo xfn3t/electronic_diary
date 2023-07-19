207f02f157a7232c2fe19bb703541d52b0651a83
Berido <sasaasihmin06@gmail.com>
2023-07-19 16:07:20 +0300

    Fixed: ID is null when post request

---
+++ src/main/java/com/elctronic/diary/controller/DiaryController.java
@@ -0,0 +1,82 @@
+package com.elctronic.diary.controller;
+
+import com.elctronic.diary.UserTable;
+import com.elctronic.diary.repo.UserTableRepository;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class DiaryController {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long counter = 0L;
+
+    @Autowired
+    private UserTableRepository studentRepo;
+
+
+    @GetMapping
+    public List<UserTable> listAll(Model model) {
+        List<UserTable> listStudents = studentRepo.findAll();
+        model.addAttribute("listStudents", listStudents);
+
+        return listStudents;
+    }
+    
+    private UserTable getMessage(Long id) {
+
+        int i = 0;
+
+        List<UserTable> listStudents = studentRepo.findAll();
+
+        // Rewrite to lambda
+        for (UserTable x : listStudents) {
+            if (x.getId().equals(id)) {
+                break;
+            }
+            i++;
+        }
+        return listStudents.get(i);
+    }
+
+    @GetMapping("{id}")
+    public UserTable getUOne(@PathVariable Long id) {
+        return getMessage(id);
+    }
+
+    @PostMapping
+    public UserTable create(@RequestBody UserTable message) {
+        
+        message.setId(counter++);
+        return studentRepo.save(message);
+    }
+
+    @PutMapping("{id}")
+    public UserTable update(@PathVariable Long id, @RequestBody UserTable message) {
+
+        UserTable messageFromDB = getMessage(id);
+
+        messageFromDB.setId(id);
+        messageFromDB.setItem(message.getItem());
+        messageFromDB.setGrade(message.getGrade());
+
+        studentRepo.save(messageFromDB);
+
+        return messageFromDB;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Long id) {
+        studentRepo.delete(getMessage(id));
+    }
+}
